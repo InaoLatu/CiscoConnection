fac028713e7cd9ad4c55cf5fa4c321c3c7a690e5
InaoLatu <inaolat@gmail.com>
2019-10-09 11:12:11 +0200

    update

--- tt3.py
+++ tt3.py
@@ -1,4 +1,3 @@
-
 from netmiko import ConnectHandler
 from datetime import datetime
 
@@ -18,7 +17,6 @@ DL = {
     'name': 'DL'
 }
 
-
 FW = {
     'device_type': 'cisco_ios',
     'host': '10.2.187.3',
@@ -26,7 +24,6 @@ FW = {
     'password': 'munics',
     'name': 'FW'
 }
-
 
 CPE = {
     'device_type': 'cisco_ios',
@@ -46,16 +43,16 @@ ISP = {
 
 cisco_devices = [AL, DL, FW, CPE, ISP]
 
+
 def main():
-
-
     for device in cisco_devices:
+        hostname = device.get('name')
+        del device['name']
         net_connect = ConnectHandler(**device)
         run_conf = net_connect.send_command("sh running-config")
-        hostname = device.get('name')
         timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         print(run_conf)
-        r = open(hostname+timestamp+".txt", "a+")
+        r = open(hostname + timestamp + ".txt", "a+")
         r.write(timestamp)
         r.write(run_conf)
 
