20a486a49121830558af9a600e140b0ffec1b5d2
InaoLatu <inaolat@gmail.com>
2019-10-09 10:30:56 +0200

    All cisco devices and timestamp.

--- tt3.py
+++ tt3.py
@@ -1,5 +1,6 @@
 
 from netmiko import ConnectHandler
+from datetime import datetime
 
 AL = {
     'device_type': 'cisco_ios',
@@ -8,13 +9,52 @@ AL = {
     'password': 'munics'
 }
 
-def main():
-    al_connect = ConnectHandler(**AL)
-    al_connect.find_prompt()
-    al_run_conf = al_connect.send_command("sh running-config")
-    print(al_run_conf)
+DL = {
+    'device_type': 'cisco_ios',
+    'host': '10.2.187.2',
+    'username': 'munics',
+    'password': 'munics'
+}
 
-    pass
+
+FW = {
+    'device_type': 'cisco_ios',
+    'host': '10.2.187.3',
+    'username': 'munics',
+    'password': 'munics'
+}
+
+
+CPE = {
+    'device_type': 'cisco_ios',
+    'host': '10.2.187.4',
+    'username': 'munics',
+    'password': 'munics'
+}
+
+ISP = {
+    'device_type': 'cisco_ios',
+    'host': '10.2.187.5',
+    'username': 'munics',
+    'password': 'munics'
+}
+
+cisco_devices = [AL, DL, FW, CPE, ISP]
+
+def main():
+
+
+    for device in cisco_devices:
+        net_connect = ConnectHandler(**device)
+        run_conf = net_connect.send_command("sh running-config")
+        hostname = net_connect.send_command("sh hostname")
+        ##Get timestamp
+        timestamp = datetime.now()
+        print(run_conf)
+        r = open(hostname+".txt", "a+")
+        r.write(timestamp)
+        r.write(run_conf)
+
 
 if __name__ == '__main__':
     main()
